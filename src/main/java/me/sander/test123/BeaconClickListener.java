448a3ab32ff09cf41a657d2fcf1cef100f2b578e
BuildTools <unconfigured@null.spigotmc.org>
2023-11-01 00:29:30 +0100

    There was a bug which made you being able to right click any block and make it a position.

--- src/main/java/me/sander/test123/BeaconClickListener.java
+++ src/main/java/me/sander/test123/BeaconClickListener.java
@@ -27,57 +27,60 @@ public class BeaconClickListener implements Listener {
     @EventHandler
     public void onBeaconClick(PlayerInteractEvent event) {
         Player player = event.getPlayer(); // Get the player
-        // Check if the player right-clicked a lodestone while holding a Nether Star
+
+        // Check if the player right-clicked a block while holding a Nether Star
         if (event.getAction().toString().contains("RIGHT")) {
             ItemStack itemInHand = event.getItem();
+
             if (itemInHand != null && itemInHand.getType() == Material.NETHER_STAR) {
-                Block lodestone = event.getClickedBlock();
-                Location location = lodestone.getLocation();
-                String worldName = location.getWorld().getName();
+                Block clickedBlock = event.getClickedBlock();
 
-                // Check if the coordinates + world are already stored
-                if (!isCoordinatesStored(location, worldName)) {
+                // Check if the clicked block is a lodestone
+                if (clickedBlock != null && clickedBlock.getType() == Material.LODESTONE) {
+                    Location location = clickedBlock.getLocation();
+                    String worldName = location.getWorld().getName();
 
-                    // yee yee ass block change here
+                    // Check if the coordinates + world are already stored
+                    if (!isCoordinatesStored(location, worldName)) {
+                        // Remove a Nether Star from the player's hand
+                        itemInHand.setAmount(itemInHand.getAmount() - 1);
 
-                    // Remove a Nether Star from the player's hand
-                    itemInHand.setAmount(itemInHand.getAmount() - 1);
+                        // Store the lodestone's location
+                        lodestoneCoordinates.add(new LodestoneCoordinate(location, worldName));
 
-                    // Store the lodestone's location
-                    lodestoneCoordinates.add(new LodestoneCoordinate(location, worldName));
+                        // Save the updated coordinates to the JSON file
+                        saveCoordinatesToJson();
 
-                    // Save the updated coordinates to the JSON file
-                    saveCoordinatesToJson();
+                        // Print the lodestone's coordinates with the world name to the console
+                        int x = location.getBlockX();
+                        int y = location.getBlockY();
+                        int z = location.getBlockZ();
+                        Bukkit.getLogger().info("Lodestone Coordinates in " + worldName + ": X=" + x + ", Y=" + y + ", Z=" + z);
 
-                    // Print the lodestone's coordinates with the world name to the console
-                    int x = location.getBlockX();
-                    int y = location.getBlockY();
-                    int z = location.getBlockZ();
-                    Bukkit.getLogger().info("Lodestone Coordinates in " + worldName + ": X=" + x + ", Y=" + y + ", Z=" + z);
+                        // Send an action bar message to the player
+                        sendActionBarMessage(player, ChatColor.GREEN + "Lodestone constructed");
 
-                    // Send an action bar message to the player
-                    sendActionBarMessage(player, ChatColor.GREEN + "Lodestone constructed");
+                        // Play a sound to all players within a 15-block radius
+                        Location soundLocation = location; // Use the location where the lodestone was constructed
 
-                    // Play a sound to all players within a 15-block radius
-                    Location soundLocation = location; // Use the location where the lodestone was constructed
-
-                    for (Player nearbyPlayer : location.getWorld().getPlayers()) {
-                        if (nearbyPlayer.getLocation().distance(soundLocation) <= 15) {
-                            // Adjust the sound parameters as needed (you can find available sounds in Sound category)
-                            nearbyPlayer.playSound(soundLocation, Sound.ENTITY_VILLAGER_WORK_WEAPONSMITH, 1.2F, 0.35F);
+                        for (Player nearbyPlayer : location.getWorld().getPlayers()) {
+                            if (nearbyPlayer.getLocation().distance(soundLocation) <= 15) {
+                                // Adjust the sound parameters as needed (you can find available sounds in Sound category)
+                                nearbyPlayer.playSound(soundLocation, Sound.ENTITY_VILLAGER_WORK_WEAPONSMITH, 1.2F, 0.35F);
+                            }
                         }
+
+                        PluginReloader.reloadPlugin(Test123.getPlugin(Test123.class));
+                        // PLACE THE METHOD HERE WHICH IS RESPONSIBLE FOR RELOADING THE PLUGIN
+                    } else {
+                        // Print "This is already a lodestone position" to the console
+                        Bukkit.getLogger().info("This is already a lodestone position");
                     }
-
-
-                    PluginReloader.reloadPlugin(Test123.getPlugin(Test123.class));
-                    // PLACE THE METHOD HERE WHICH IS RESPONSIBLE FOR RELOADING THE PLUGIN
-                } else {
-                    // Print "This is already a lodestone position" to the console
-                    Bukkit.getLogger().info("This is already a lodestone position");
                 }
             }
         }
     }
+
 
     // Send an action bar message to the player
     private void sendActionBarMessage(Player player, String message) {
