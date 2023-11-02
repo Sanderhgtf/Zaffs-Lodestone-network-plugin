4b25899d13c7a875d6c2b69084b7a86b0c617247
BuildTools <unconfigured@null.spigotmc.org>
2023-11-02 09:25:54 +0100

    changes to gui + teleport effect

--- src/main/java/me/sander/test123/CustomInventoryGui.java
+++ src/main/java/me/sander/test123/CustomInventoryGui.java
@@ -1,17 +1,24 @@
 package me.sander.test123;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.Particle;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
+import java.util.Collections;
 import java.util.List;
 
 public class CustomInventoryGui implements Listener {
@@ -19,7 +26,7 @@ public class CustomInventoryGui implements Listener {
     private final List<LodestoneCoordinate> lodestoneCoordinates;
 
     public CustomInventoryGui(List<LodestoneCoordinate> lodestoneCoordinates) {
-        inventory = Bukkit.createInventory(null, 27, "Custom GUI");
+        inventory = Bukkit.createInventory(null, 27, "LODESTONE NETWORK");
         this.lodestoneCoordinates = lodestoneCoordinates;
         loadNetherStarNames();
     }
@@ -39,12 +46,12 @@ public class CustomInventoryGui implements Listener {
     public void onInventoryClick(InventoryClickEvent event) {
         if (event.getClickedInventory() == inventory) {
             ItemStack clickedItem = event.getCurrentItem();
-            if (clickedItem != null && clickedItem.getType() == Material.CALCITE) {
-                String displayName = clickedItem.getItemMeta().getDisplayName();
+            if (clickedItem != null && clickedItem.getType() == Material.COMPASS) {
+                String displayName = ChatColor.stripColor(clickedItem.getItemMeta().getDisplayName());
                 Player player = (Player) event.getWhoClicked();
 
                 for (LodestoneCoordinate coordinate : lodestoneCoordinates) {
-                    if (coordinate.getNetherStarName().equals(displayName)) {
+                    if (ChatColor.stripColor(coordinate.getNetherStarName()).equals(displayName)) {
                         String message = String.format("%s clicked: X=%d, Y=%d, Z=%d in World: %s",
                                 player.getName(),
                                 coordinate.getLocation().getBlockX(),
@@ -57,6 +64,15 @@ public class CustomInventoryGui implements Listener {
                         Location targetLocation = coordinate.getLocation().clone().add(0.5, 1, 0.5);
                         player.teleport(targetLocation);
 
+                        // Create a dark smoke particle effect
+                        for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
+                            onlinePlayer.spawnParticle(Particle.SMOKE_NORMAL, targetLocation, 100, 0.5, 1, 0.5, 0.1);
+                        }
+
+                        // Give the player the darkness effect for 2 seconds
+                        PotionEffect darknessEffect = new PotionEffect(PotionEffectType.BLINDNESS, 20, 1);
+                        player.addPotionEffect(darknessEffect);
+
                         event.setCancelled(true); // Prevent item movement
                         break;
                     }
@@ -65,21 +81,29 @@ public class CustomInventoryGui implements Listener {
         }
     }
 
-
-
-
-
     public void open(Player player) {
         player.openInventory(inventory);
     }
 
     public void loadNetherStarNames() {
-        // Fill the inventory with Calcite blocks named after NetherStarNames
+        // Fill the inventory with Compass items named after NetherStarNames without enchant name
         for (LodestoneCoordinate lodestone : lodestoneCoordinates) {
             String netherStarName = lodestone.getNetherStarName();
-            ItemStack item = new ItemStack(Material.CALCITE);
+            ItemStack item = new ItemStack(Material.COMPASS);
             ItemMeta itemMeta = item.getItemMeta();
-            itemMeta.setDisplayName(netherStarName);
+
+            // Apply the enchantment effect without any enchant name
+            itemMeta.addEnchant(Enchantment.ARROW_INFINITE, 1, true);
+
+            // Create a custom ItemFlag to hide the enchantment name
+            itemMeta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+
+            // Set the display name with custom chat color
+            itemMeta.setDisplayName(ChatColor.YELLOW + netherStarName);
+
+            // Add lore with custom chat color
+            itemMeta.setLore(Collections.singletonList(ChatColor.GRAY + "Click to teleport"));
+
             item.setItemMeta(itemMeta);
             inventory.addItem(item);
         }
